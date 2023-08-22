94d4d71aa2992504b7a18b159a1ef5be1401a146
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2023-08-21 23:21:07 -0500

    Refactoring with Project Lombok

--- src/main/java/com/springrecipe/springrecipe/bootstrap/DataLoader.java
+++ src/main/java/com/springrecipe/springrecipe/bootstrap/DataLoader.java
@@ -4,6 +4,7 @@ import com.springrecipe.springrecipe.domain.*;
 import com.springrecipe.springrecipe.repositories.CategoryRepository;
 import com.springrecipe.springrecipe.repositories.RecipeRepository;
 import com.springrecipe.springrecipe.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -29,6 +31,7 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("Loading Bootstrap Data");
     }
 
     private List<Recipe> getRecipes() {
