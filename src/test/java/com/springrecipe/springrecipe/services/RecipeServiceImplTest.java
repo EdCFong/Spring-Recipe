6c956ad1996d72566519f4383f92ff9bd89711b7
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2023-08-31 21:49:03 -0500

    Using Mockito Mocks

---
+++ src/test/java/com/springrecipe/springrecipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.springrecipe.springrecipe.services;
+
+import com.springrecipe.springrecipe.domain.Recipe;
+import com.springrecipe.springrecipe.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        //MockitoAnnotations.initMocks(this); -->  deprecated
+        MockitoAnnotations.openMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet<>();
+        recipesData.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
